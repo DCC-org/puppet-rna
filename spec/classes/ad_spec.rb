f2405d75a678e2a719eeaa21eea52f87d25bb959
Tim Meusel <tim@bastelfreak.de>
2018-04-03 16:05:48 +0200

    add basic spec tests for samba

---
+++ spec/classes/ad_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'rna::ad' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      let :pre_condition do
+        'include rna'
+      end
+      describe 'with defaults' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('rna::ad') }
+        it { is_expected.to contain_class('samba') }
+     end
+    end
+  end
+end
